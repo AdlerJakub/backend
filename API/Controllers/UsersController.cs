df694c6b34375bffd984206c2e8ae7359bd914fc
Jakub Adler <jakub.adler@asseco.pl>
2020-11-01 13:32:19 +0100

    Dodano autoryzację po stronie backendu.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,12 +4,11 @@ using System.Collections.Generic;
 using API.Entities;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,11 +18,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(int id)
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+
+namespace API.Data.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
